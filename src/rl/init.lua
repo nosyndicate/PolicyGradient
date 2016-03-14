2403233b5ff74a8bda27f4e2bd2b6ab4f4cd8c7f
nosyndicate <nosyndicate@gmail.com>
2016-03-14 18:26:22 -0400

    add compatiblefeature

--- src/rl/init.lua
+++ src/rl/init.lua
@@ -9,6 +9,7 @@ torch.include('rl','Reinforce.lua');
 torch.include('rl','GPOMDP.lua');
 torch.include('rl','Policy.lua');
 torch.include('rl','SoftmaxPolicy.lua');
+torch.include('rl','GaussianPolicy.lua');
 torch.include('rl','Optimizer.lua');
 torch.include('rl','StochasticGradientDescent.lua');
 
