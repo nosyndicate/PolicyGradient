e30563a8fa51d7afa2e61592c0512aec4e4cd8f5
nosyndicate <nosyndicate@gmail.com>
2016-04-20 23:30:55 -0400

    add TileCoding

--- src/rl/TileCoding.lua
+++ src/rl/TileCoding.lua
@@ -1,15 +1,58 @@
+
 require 'torch'
+require 'utils'
 
--- this is the parent class of SoftmaxCompatibleFeature and GaussianCompatibleFeature
+--[[
+	This is an implementation of grid-style tile codings, heavily based on the python code from
+	http://rlai.cs.ualberta.ca/RLAI/RLtoolkit/tiles.html
+--]]
 
-local TileCoding, parent = torch.class('rl.TileCoding','rl.Feature');
+local TileCoding, parent = torch.class('rl.TileCoding','rl.Feature')
 
 
-function CompatibleFeature:__init()
-
+function TileCoding:__init()
+	self.maxNumFloats = 20
+	self.maxLongInteger = 2147483647
+	self.maxLongIntegerBy4 = math.floor(self.maxLongInteger/4)
+	self.randomTable = {}
+	
+	for i = 1,2048 do
+		self.randomTable[i] = 
+	end
+	
+	self.qstate = {}
+	self.base = {}
+	for i = 1,self.maxNumFloats do
+		self.qstate[i] = 0
+		self.base[i] = 0
+	end
+	
 end
+
+
 
 -- this function compute the feature value given the state and action
-function CompatibleFeature:feature(s)
+function TileCoding:feature(s)
 	return s
 end
+
+-- Does initial assignments to coordinates
+function startTiles(coordinations, numTilings, floats, ints)
+	local numFloats = #floats
+	local i = numFloats + 1
+	-- first place the integer value
+	for i = 1,#ints do
+		coordinations[offset + i] = int
+end
+
+function tiles(numTilings, floats, ints)
+	ints = ints or {} -- if ints is not used, initialize to empty table
+	local numFloats = #floats
+	local numCoordination = 1 + numFloats + #ints
+	local coordination = {}
+	
+	self:startTiles(coordination, numTilings, floats, ints)
+end
+
+
+
