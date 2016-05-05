35955ab37d66ee943dcefef607845fbfdfe0d57d
nosyndicate <nosyndicate@gmail.com>
2016-05-04 21:36:28 -0400

    start actor critic

--- src/rl/ActorCritic.lua
+++ src/rl/ActorCritic.lua
@@ -3,19 +3,24 @@ require 'torch'
 -- Implementation of incremental actor critic, using Sarsa critic
 -- based on policy gradient lecture of David Silver 
 -- http://www0.cs.ucl.ac.uk/staff/d.silver/web/Teaching_files/pg.pdf
--- and nips paper "Incremental Natural Actor-Critic Algorithms"
--- https://papers.nips.cc/paper/3258-incremental-natural-actor-critic-algorithms.pdf
-
-local ActorCritic, parent = torch.class('rl.Reinforce','rl.PolicySearch')
 
 
-function ActorCritic:__init(model, actor, critic, optimizer)
+local ActorCritic, parent = torch.class('rl.ActorCritic','rl.PolicySearch')
+
+
+function ActorCritic:__init(model, actor, optimizer, qCritic, vCritic)
 	-- parent method have to be called this way: with dot and pass self as first parameters
 	parent.__init(self, model)
 	self.actor = actor
-	
+	self.qCritic = qCritic
+	self.vCritic = vCritic	
 	self.optimizer = optimizer
-	--self.optimizer.params:uniform(-0.08,0.08)
+	
+	-- learning rate for q critic
+	self.beta = 0.001
+	
+	-- inspect which policy we are using
+	
 end
 
 function ActorCritic:setLearningRate(alpha)
@@ -23,11 +28,32 @@ function ActorCritic:setLearningRate(alpha)
 	self.optimizer:setLearningRate(self.alpha)
 end
 
-function ActorCritic:step(s, r)
+function ActorCritic:setQCriticLearningRate(beta)
+	self.beta = beta
+end
 
+function ActorCritic:step(s, r)
+	assert(false, "Actor Critic is an incremental method, should not call this method, learn function should be called instead")
+end
+
+-- do some work before each episode start
+function ActorCritic:startTrial()
+	self.action = nil
+end
+
+-- do some work after the episode ends
+function ActorCritic:endTrial()
+	
 end
 
 function ActorCritic:getAction(s)
+	if not self.action then
+		self.action = self:getNextAction(s)
+	end
+	return self.action
+end
+
+function ActorCritic:getNextAction(s)
 	-- get the parameters for the distribution of the stochastic policy
 	local parameters = self.model:forward(s)
 	--print(parameters)
@@ -36,16 +62,33 @@ function ActorCritic:getAction(s)
 	return action
 end
 
-function ActorCritic:learn(s, r)
-	local gradient = self:calculateGradient(s, r)
+function ActorCritic:learn(s, r, sprime)
+	-- this is on policy actor critic, thus, we are using a SARSA critic
+	local aprime = self:getNextAction(sprime)
 	
-	-- update the parameters with the gradient
-	self.optimizer:gradientAscent(gradient)
+	-- compute the quantities for the critic
+	local featurePrime = self.actor:compatibleFeature(sprime, aprime)
+	local feature = self.actor:compatibleFeature(s, self.action)
+	local qSprimeAprime = self.qCritic:foward(featurePrime)
+	local qsa = self.qCritic:forward(feature)
+	
+	-- compute the td error
+	local td = r + self.gamma*qSprimeAprime-qsa
+	
+	-- compute the gradient of the actor parameters
+	self.optimizer.grads:zero()
+	local dLogPolicyDOutput = self.actor:backward()
+	self.model:backward(s, dLogPolicyDOutput)
+	-- instead using the direct reward, we use the Q-value
+	local tempGradient = torch.cmul(self.optimizer.grads:clone(), qsa)
+	
+	self.optimizer:gradientAscent(tempGradient)
+	
+	-- set the next action
+	self.action = aprime
 end
 
-function ActorCritic:calculateGradient(s, r)
-	
-end
+
 
 
 
